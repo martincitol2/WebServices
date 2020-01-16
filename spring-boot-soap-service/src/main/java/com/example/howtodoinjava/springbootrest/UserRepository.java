4a359f7ceb3b0b7fab5a6f0c4a84981df6e49a39
Martin Patino <martin_patino@intranet.itresources>
2020-01-16 16:39:02 -0300

    Version Refactorizada

--- spring-boot-soap-service/src/main/java/com/example/howtodoinjava/springbootrest/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.howtodoinjava.springbootrest;
-
-
-import org.springframework.data.repository.CrudRepository;
-
-    public interface UserRepository extends CrudRepository<User, Integer> {
-}
